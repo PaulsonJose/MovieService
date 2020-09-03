1e0c7aab307d2005cad2fb49767ac0b3aa09f2ec
PaulsonJose <69240159+PaulsonJose@users.noreply.github.com>
2020-09-03 23:45:51 +0200

    Added Hystrix updates

--- movie-catalog-service/src/main/java/io/javamovie/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/javamovie/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,21 +2,6 @@ package io.javamovie.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.client.loadbalancer.LoadBalanced;
-import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
-
-@SpringBootApplication
-public class MovieCatalogServiceApplication {
-
-	@Bean
-	@LoadBalanced
-	public RestTemplate getRestTemplate() {
-		return new RestTemplate();
-	};package io.javamovie.moviecatalogservice;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.loadbalancer.LoadBalanced;
 import org.springframework.cloud.netflix.hystrix.dashboard.EnableHystrixDashboard;
@@ -33,13 +18,6 @@ public class MovieCatalogServiceApplication {
 	public RestTemplate getRestTemplate() {
 		return new RestTemplate();
 	};
-	
-	public static void main(String[] args) {
-		SpringApplication.run(MovieCatalogServiceApplication.class, args);
-	}
-
-}
-
 	
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
