aee335053db9a844265a6606a6f2a754682234fa
PaulsonJose <69240159+PaulsonJose@users.noreply.github.com>
2020-09-03 23:45:01 +0200

    Seperated the API calles in to seperate services

--- movie-catalog-service/src/main/java/io/javamovie/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/javamovie/moviecatalogservice/resources/MovieCatalogResource.java
@@ -7,33 +7,30 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
 import io.javamovie.moviecatalogservice.models.CatalogItem;
 import io.javamovie.moviecatalogservice.models.Movie;
+import io.javamovie.moviecatalogservice.models.Rating;
 import io.javamovie.moviecatalogservice.models.UserRating;
+import io.javamovie.moviecatalogservice.services.MovieInfo;
+import io.javamovie.moviecatalogservice.services.UserRatingInfo;
 
 @RestController
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 	
 	@Autowired
-	private RestTemplate restTemplate;
+	MovieInfo movieInfo;
+	
+	@Autowired
+	UserRatingInfo userRatingInfo;
 
 	@RequestMapping("/{userId}")
 	public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
 		
+		UserRating ratings = userRatingInfo.getUserRating(userId);
 		
-		UserRating ratings = restTemplate.getForObject("http://ratings-data-service/ratingsdata/users/" + userId, UserRating.class);
-		
-		return ratings.getUserRating().stream().map(rating -> {
-			//For each movie ID, call movie info service and get details
-			Movie movie = restTemplate.getForObject("http://movie-info-service/movies/" + rating.getMovieId(), Movie.class);
-			//Put them all together
-		return new CatalogItem(rating.getMovieId(), movie.getName(), movie.getOverview(), rating.getRating());
-		})
+		return ratings.getUserRating().stream().map(rating -> movieInfo.getCatalogItem(rating))
 				.collect(Collectors.toList());
-		
-
 	}
 }
